2791cf684fab747c26c5023604e6dffca6a66702
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-06-17 03:54:37 -0700

    wage for month

--- EmpWelcome.java
+++ EmpWelcome.java
@@ -2,19 +2,23 @@
 public class EmpWelcome {
      
    private static final int IsFullTime = 1;
-   private static final int EmpPerHr = 20;
    private static final int IsPartTime = 2;
+   private static final int EmpPerHr = 20;
+   private static final int WorkingDays = 2;
 
    public static void main(String[] args) {
 
        checkEmp();
    }
    public static void checkEmp() {
+        //variables
        int empHr = 0;
+       int totalWage = 0;
        int empWage = 0;
 
-       int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-       switch (empCheck) {
+       for (int day = 0; day<WorkingDays; day++) {
+           int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+           switch (empCheck) {
            case IsPartTime:
                empHr = 4;
                break;
@@ -25,6 +29,11 @@ public class EmpWelcome {
                empHr = 0;
        }
        empWage = empHr * EmpPerHr;
+       totalWage += empWage;
        System.out.println("Emp Wage: " + empWage);
+    }
+    System.out.println("Total EmpWage: " + totalWage);
    }
 }
+     
+   
