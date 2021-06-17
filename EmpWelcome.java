658a5bb09c7be823b5ea8a11c67af4baed0c01d1
7506574455 <84278635+7506574455@users.noreply.github.com>
2021-06-17 02:00:21 -0700

    Dailywage

--- EmpWelcome.java
+++ EmpWelcome.java
@@ -1,13 +1,19 @@
 
 public class EmpWelcome {
 
-   public static void main(String[] args) {
+     public static void main(String[] args) {
              //constants
        int IS_FULL_TIME = 1;
+       int EMP_PER_HR = 20;
+           //variables
+       int empHr = 0;
+       int empWage = 0;
        double empCheck = Math.floor(Math.random() * 10) % 2;
        if (empCheck == IS_FULL_TIME)
-           System.out.println("Employee is Present");
+           empHr = 8;
        else
-           System.out.println("Employee is Absent");
+           empHr = 0;
+       empWage = empHr * EMP_PER_HR;
+           System.out.println("Emp Wage: " + empWage);
    }
 }
